3c11a1e29c6b08d468ad2a4f737899783bbb9fce
lovemefan <450489712@qq.com>
2021-09-09 14:14:10 +0800

    协程分布式爬虫

---
+++ website/baike/baidubaike/BaiduBaikeCrawler.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+# @Time  : 2021/9/9 2:15
+# @Author : lovemefan
+# @Email : lovemefan@outlook.com
+# @File : BaiduBaikeCrawler.py
+import asyncio
+import re
+
+import aiohttp
+
+from website.baike.crawlerBase import CrawlerBaike
+
+
+class BaiduBaike(CrawlerBaike):
+    """收集百度百科并链接存入数据库，并爬取链接中的文本"""
+    def __init__(self, url: str):
+        super().__init__(url)
+
+    async def crawler_baike_content(self):
+        # todo 爬取内容
+        pass
+
+    async def get_baike_url_list(self, url: str, session: None):
+        html = await self.get_request(url, session)
+        url_list = re.findall('((https://baike.baidu.com)?/item/?.*?)"', html)
+
+        url_list = [item[0].replace("\\/", "/") if len(item[1]) != 0
+                    else "https://baike.baidu.com%s" % item[0].replace("\\/", "/").replace("\\", "") for item in url_list]
+        # 去重
+        url_list = set(url_list)
+        return url_list
+
+    async def crawler_baike_urls(self):
+        async with aiohttp.ClientSession() as session:
+            while await self.redis.llen(self.url_queue):
+                top_url = await asyncio.wait_for(self.redis.lpop(self.url_queue), 10)
+                url_list = await self.get_baike_url_list(str(top_url, encoding="utf-8"), session)
+                print(f"以获取{len(url_list)}条链接")
+                for url in url_list:
+                    print(f"正在加入{url}")
+                    await self.add_url_to_redis(url)
+
+
+if __name__ == '__main__':
+    baidubaike = BaiduBaike("redis://localhost/0")
+    baidubaike.run()
---
+++ website/baike/crawlerBase.py
@@ -0,0 +1,105 @@
+# -*- coding: utf-8 -*-
+# @Time  : 2021/9/9 1:31
+# @Author : lovemefan
+# @Email : lovemefan@outlook.com
+# @File : crawler.py
+import abc
+import asyncio
+
+import aiohttp
+import aioredis
+from aiohttp import ClientResponse
+from aioredis import Redis
+
+
+class CrawlerBaike:
+    """百科爬虫父类，
+     1. 规定crawler_baike_urls为爬取链接
+     2. 规定crawler_baike_content根据uri为爬取内容
+     继承该类需要重写：
+        * crawler_baike_urls() # 爬取urls链接存入reids的业务逻辑
+
+        * crawler_baike_content(uri: str) #根据某个uri 爬取 网站内容的业务逻辑
+
+        * run() # 总业务逻辑
+     """
+
+    def __init__(self, url: str):
+        self.redis = None
+        self.url = url
+        self.loop = asyncio.get_event_loop()
+        self.url_set = "URL"
+        self.url_queue = "URL_QUEUE"
+
+    async def connect(self):
+        """
+        建立与reids的连接
+        :return:
+        """
+        if self.redis is None:
+            self.redis: Redis = await aioredis.from_url(self.url)
+
+    async def get_request(self, url: str, session: None) -> ClientResponse:
+        """
+        get 网络请求
+        :param session:
+        :param url: 网路url
+        :return:  网络response
+        """
+        if session is None:
+            async with aiohttp.ClientSession() as session:
+                async with session.get(url) as response:
+                    return await response.text()
+        else:
+            async with session.get(url) as response:
+                return await response.text()
+
+
+    @abc.abstractmethod
+    async def crawler_baike_content(self):
+        """
+        根据分发到的百科uri任务， 将文本爬取下来
+        :return:
+        """
+        pass
+
+    @abc.abstractmethod
+    async def crawler_baike_urls(self):
+        """
+        爬取 百科将url存入redis的set中保证不重复
+        :return:
+        """
+        # 插入测试
+        await self.add_url_to_redis("sina.com")
+
+    async def add_url_to_redis(self, url: str):
+        """
+        将url添加到redis中去重,
+        redis 中会维护一个名为`URL`的集合 和一个为 `URL_QUEUE` 的list队列
+        :param url:
+        :return:
+        """
+        # 如果当前uri不在集合中着插入队列和集合中
+        if await self.redis.sismember(self.url_set, url) == 0:
+            await self.redis.rpush(self.url_queue, url)
+            await self.redis.sadd(self.url_set, url)
+
+    def run(self):
+        self.loop.run_until_complete(self.connect())
+        self.loop.run_until_complete(
+            asyncio.gather(
+                *[
+                    self.crawler_baike_urls(),
+                    self.crawler_baike_content()
+                ]
+                )
+        )
+
+
+
+
+if __name__ == '__main__':
+    # 测试
+    crawler = CrawlerBaike("redis://localhost/0")
+    crawler.run()
+
